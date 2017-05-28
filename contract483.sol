dc27743831d6ff29ba95ef0861782e1ce3d7b408
Mavera07 <cyberoyal@windowslive.com>
2017-05-28 19:33:02 +0300

    Solidty contract for cmpe483 hw2 and hw3. withdraw will be implemented. endDates will be checked. consecutive treatments in a short period will be prohibited.

---
+++ contract483.sol
@@ -0,0 +1,133 @@
+pragma solidity ^0.4.11;
+
+contract SSInsurance {
+    
+    struct Customer {	
+	    address	owner;	
+        uint endDate;
+        bool isPaid;
+        uint limit;
+        uint remaining;
+    }	
+    
+    struct Hospital {	
+	    address	owner;	
+		uint endDate;
+		bool isPaid;
+		uint treatingCost;
+    }	
+    
+    struct Statistics {
+        uint noCustomer;
+        uint noHospital;
+    }
+    
+    
+    mapping(address => Customer) customers;
+    mapping(address => Hospital) hospitals;
+    Statistics statistics;
+    
+    
+    
+    // Customer API Call
+    // This function takes parameters from customers; duration and payment.
+    // From the desired insurance duration and desired insurance payment,
+    // it calculates a multiplier number, in other words insurance note
+    // With this number, insurance limit will be calculated.
+    // insurance limit = payment * multiplier / 100
+    function getInsuranceNote(address cust1,uint duration,uint payment)
+        returns (uint multiplier){
+            
+        if(duration <= 12){
+            multiplier =  400 - (400 - 300) * (duration - 0) / (12 - 0);
+        }else if (duration <= 24){
+            multiplier = 300 - (300 - 240) * (duration -12) / (24 - 12);
+        }else if (duration <= 36){
+            multiplier = 240 - (240 - 200) * (duration -24) / (36 - 24);
+        }else if (duration <= 60){
+            multiplier = 200 - (200 - 140) * (duration -36) / (60 - 36);
+        }else{
+            multiplier = 140;
+        }
+        
+    }
+    
+    // Customer API Call
+    // This function returns the number of hospitals that have contract
+    // with our insurance company
+    function getHospitalNumber()
+        constant returns (uint hospitalNumber){
+            hospitalNumber = statistics.noHospital;
+    }
+    
+    // Customer API Call
+    // This function is for the insurance payment
+    // Customers call this function to buy an insurance
+    function payInsurance(uint duration) payable{
+        require(!customers[msg.sender].isPaid);
+        
+        customers[msg.sender].endDate = now + ( duration * 1 minutes );
+        customers[msg.sender].isPaid = true;
+        
+        uint mult = getInsuranceNote(msg.sender,duration,msg.value);
+        customers[msg.sender].limit = msg.value*mult/100;
+        customers[msg.sender].remaining = 0;
+    }
+    
+    // Hospital API Call
+    // TO DO
+    function hospitalOffer (uint treatingCost) payable
+        returns (bool){
+            require(!hospitals[msg.sender].isPaid);
+            
+            uint duration = 12;
+            uint hospitalProfit = statistics.noCustomer * 6 / 10 *treatingCost * 6 / 10;
+            
+            uint sharePercent = 0;
+            if(statistics.noHospital < 10){
+                sharePercent = 20 + (40 - 20) * statistics.noHospital / 10;
+            }else if(statistics.noHospital < 100){
+                sharePercent = 40 + (50 - 40) * (statistics.noHospital-10) / (100 - 10);
+            }else if(statistics.noHospital < 150){
+                sharePercent = 50 - (50 - 30) * (statistics.noHospital-100) / (150 - 100);
+            }else{
+                sharePercent = 30;
+            }
+        
+            uint desiredGain = hospitalProfit * sharePercent / 100;
+            
+            require(desiredGain <= msg.value);
+            
+            hospitals[msg.sender].endDate = now + duration * ( 1 minutes ); 
+            hospitals[msg.sender].isPaid = true;
+            hospitals[msg.sender].treatingCost = treatingCost;
+            
+            return true;
+        
+    }
+    
+    // Hospital API Call
+    // This function returns the number of customers that have
+    // contract with our insurance company.
+    function getCustomerNumber ()
+        returns (uint customerNumber){
+            customerNumber = statistics.noCustomer;
+    }
+    
+    
+    
+    // Customer API Call
+    // Hospital gets money from our insurance company for treaing a customer
+    // Customer must run this code to be treated by the hospital
+    // Hospital will not treat the customer before the customer run this function
+    function  withdraw()
+        returns (bool){
+            
+        
+    }
+    
+    // TODO
+    // Check date for updating isPaid
+    
+    
+}
